b5df534f823a6a7b848cd7188d142ba44ee5ac67
Matei Sîrbu <matei.sirbu@student.unitbv.ro>
2021-04-16 13:06:10 +0300

    Corrected shell scripts

--- sh/nativefy-messenger.sh
+++ sh/nativefy-messenger.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 npm update nativefier
-nativefier --name "Messenger" --arch "x64" --background-color "#131313" --bounce --counter --icon "../icns/Messenger.icns" --darwin-dark-mode-support --fast-quit "https://messenger.com"
+nativefier --name "Messenger" --arch "x64" --background-color "#131313" --bounce --counter --icon "./icns/Messenger.icns" --darwin-dark-mode-support --fast-quit "https://messenger.com"
